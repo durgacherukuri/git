6965b8b708e24a6b265562ae69f8fe5d9a366b00
durgacherukuri <34650905+durgacherukuri@users.noreply.github.com>
2017-12-18 23:30:59 +0530

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,13 @@
+package task2;
+	import java.util.Scanner;
+	public class task{
+	     public static void main(String []args){
+	         Scanner sc=new Scanner(System.in);
+	         System.out.println("enter number n");
+	        int n=sc.nextInt();
+	        /*System.out.println("enter number y");
+	        int y=sc.nextInt();*/
+	         int mul=n>>2;
+	        System.out.println("multiplication:" +mul);
+	     }
+	}
