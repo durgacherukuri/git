6ec230ec23a14e31fad5d58882fbf835e36c78d8
durgacherukuri <34650905+durgacherukuri@users.noreply.github.com>
2017-12-18 23:31:17 +0530

    Add files via upload

---
+++ task3.java
@@ -0,0 +1,19 @@
+package task3;
+
+import java.util.Scanner;
+
+public class task3 {
+	public static void main(String args[])
+	{
+		Scanner sc =new Scanner(System.in);
+		String fname = "Durga";
+		String lname="Cherukuri";
+		String designtn="Graduate";
+		int dob=10/11/1995;
+		System.out.println("fristname is:"+fname);
+		System.out.println("last name is:"+lname);
+		System.out.println("desigination:"+designtn);
+		System.out.println("date of brith:"+dob);
+		
+	}
+}
